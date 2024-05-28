cad66684b4de2c4ab8388ce535c6ffc46768da9f
Dylan Silva <c21088964@cardiff.ac.uk>
2024-05-28 22:25:36 +0100

    project init

---
+++ src/renderer/src/App.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <img alt="logo" class="logo" src="./assets/balls.jpg" />
+  <div class="creator">A picture of Dylan's Balls</div>
+  <div class="text">
+    Ride on Dylans' Dick with
+    <span class="vue">Vue</span>
+    and
+    <span class="ts">TypeScript</span>
+  </div>
+  <p class="tip">Please try pressing <code>F12</code> to open the devTool</p>
+  <div class="actions">
+    <div class="action">
+      <a href="https://iamdylan.co.uk" target="_blank" rel="noreferrer">Documentation for Dylan</a>
+    </div>
+  </div>
+  
+</template>
